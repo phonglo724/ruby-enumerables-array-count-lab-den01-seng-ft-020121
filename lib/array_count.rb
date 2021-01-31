4a4128c90f4c9bcf7a1644852e0600294408840e
Phong Lo <phonglo724@gmail.com>
2021-01-31 22:36:34 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,16 +1,16 @@
-# def count_strings(array)
-#   element = 0
-#   array.count do |element|
-#       element.class == String
-#   end
-# end
+def count_strings(array)
+  element = 0
+  array.count do |element|
+      element.class == String
+  end
+end
 
-# def count_empty_strings(array)
-#     element = 0
-#   array.count do |element|
-#       element == ""
-#   end
-# end
+def count_empty_strings(array)
+    element = 0
+  array.count do |element|
+      element == ""
+  end
+end
 
 #or
 
